d5de49bd617ea43b614af36e9f9a65877c4ff66d
Michael La Plante <michael@michaellaplante.com>
2013-09-07 11:34:44 -0400

    adding movie data class

---
+++ week3Challenge/LPMovieClass.h
@@ -0,0 +1,13 @@
+//
+//  LPMovieClass.h
+//  week3Challenge
+//
+//  Created by Michael La Plante on 9/7/13.
+//  Copyright (c) 2013 Michael La Plante. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LPMovieClass : NSObject
+
+@end
